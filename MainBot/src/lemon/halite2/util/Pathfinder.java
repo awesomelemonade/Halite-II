9a51429d72a4233837faf53a3089b4bb5258b653
awesomelemonade <awesomelemonade@gmail.com>
2017-10-27 23:24:19 -0400

    Weird Finnicking of Pathfinding just to get higher rankings and not actually fixing the bugs

--- MainBot/src/lemon/halite2/util/Pathfinder.java
+++ MainBot/src/lemon/halite2/util/Pathfinder.java
@@ -1,5 +1,9 @@
 package lemon.halite2.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import hlt.Entity;
 import hlt.GameConstants;
 import hlt.GameMap;
 import hlt.Planet;
@@ -20,8 +24,14 @@ public class Pathfinder {
 	public static ThrustMove pathfind(Ship ship, Position start, Position end, double buffer) {
 		return pathfind(ship, start, end, buffer, RoundPolicy.ROUND);
 	}
+	private static int lastShipId = -1;
+	private static List<Entity> entitiesPathfinded = new ArrayList<Entity>();
 	//Offset Policy: -1 = OFFSET NEGATIVE DIR; 1 = OFFSET POSITIVE DIR; 0 = NO PREFERENCE
 	public static ThrustMove pathfind(Ship ship, Position start, Position end, double buffer, RoundPolicy offsetPolicy) {
+		if(lastShipId!=ship.getId()) {
+			lastShipId = ship.getId();
+			entitiesPathfinded.clear();
+		}
 		if(offsetPolicy==RoundPolicy.NONE){
 			throw new IllegalArgumentException("OffsetPolicy cannot be NONE");
 		}
@@ -33,35 +43,39 @@ public class Pathfinder {
 		
 		//Continue Law of Sines if buffer not equal to 0
 		double targetDistance = 0;
-		if(buffer>realDistance*Math.sin(offsetDirection)){ //Ensures triangle to be solvable
+		/*if(buffer>realDistance*Math.sin(offsetDirection)){ //Ensures triangle to be solvable
 			double lawOfSinesValue = Math.sin(offsetDirection)/buffer; //Divide by 0 error if buffer = 0
 			double sineInverse = Math.PI-Math.asin(realDistance*lawOfSinesValue); //It's the one that is greater than Math.PI/2 radians
 			targetDistance = Math.sin(Math.PI-sineInverse-offsetDirection)/lawOfSinesValue;
 			//targetDistance = Math.sin(Math.asin(realDistance*lawOfSinesValue)-offsetDirection)/lawOfSinesValue;
 		}else{
-			targetDistance = realDistance*Math.cos(offsetDirection); //Not even sure if this ever happens
-		}
+			targetDistance = realDistance*Math.cos(offsetDirection);
+		}*/
+		targetDistance = 7;
 		Position targetPosition = start.addPolar(targetDistance, targetDirection);
 		//Target Vector to travel: (targetDistance, targetDirection) @ targetPosition
 		for (Planet planet : gameMap.getPlanets()) {
-			ThrustMove move = testCollision(ship, start, targetPosition, targetDirection, planet.getPosition(), planet.getRadius());
+			ThrustMove move = testCollision(ship, start, targetPosition, targetDirection, planet.getPosition(), planet.getRadius(), planet);
 			if(move!=null) {
 				return move;
 			}
 		}
-		/*for (Ship otherShip: gameMap.getShips()) {
+		for (Ship otherShip: gameMap.getShips()) {
 			if(otherShip.getOwner()==gameMap.getMyPlayerId()&&otherShip.getId()!=ship.getId()) {
-				ThrustMove move = testCollision(ship, start, targetPosition, targetDirection, otherShip.getPosition(), otherShip.getRadius());
+				ThrustMove move = testCollision(ship, start, targetPosition, targetDirection, otherShip.getPosition(), otherShip.getRadius(), otherShip);
 				if(move!=null) {
 					return move;
 				}
 			}
-		}*/
+		}
 		return new ThrustMove(ship, (int)Math.min(targetDistance, 7), targetDirection, RoundPolicy.NONE);
 	}
-	public static ThrustMove testCollision(Ship ship, Position start, Position targetPosition, double targetDirection, Position position, double radius) {
+	public static ThrustMove testCollision(Ship ship, Position start, Position targetPosition, double targetDirection, Position position, double radius, Entity entity) {
 		double calculatedBuffer = radius+GameConstants.SHIP_RADIUS+FUDGE_FACTOR;
 		if(Geometry.segmentCircleIntersection(start, targetPosition, position, calculatedBuffer)) {
+			if(entitiesPathfinded.contains(entity)) {
+				return new ThrustMove(ship, 0, 0, RoundPolicy.NONE);
+			}
 			double distance = start.getDistanceTo(position);
 			if(distance<=calculatedBuffer){
 				//you're in the planet :(
@@ -72,14 +86,15 @@ public class Pathfinder {
 			double magnitude = Math.sqrt(start.getDistanceSquared(position)-radius*radius);
 			double direction = start.getDirectionTowards(position);
 			RoundPolicy newOffsetPolicy = RoundPolicy.NONE;
-			if(Math.abs((direction-tangentValue)-targetDirection)<Math.abs((direction+tangentValue)-targetDirection)){
+			//if(Math.abs((direction-tangentValue)-targetDirection)<Math.abs((direction+tangentValue)-targetDirection)){
 				direction+=tangentValue;
 				newOffsetPolicy = RoundPolicy.CEIL;
-			}else{
-				direction-=tangentValue;
-				newOffsetPolicy = RoundPolicy.FLOOR;
-			}
+			//}else{
+			//	direction-=tangentValue;
+			//	newOffsetPolicy = RoundPolicy.FLOOR;
+			//}
 			Position endPoint = start.addPolar(magnitude, direction);
+			entitiesPathfinded.add(entity);
 			return pathfind(ship, start, endPoint, 0, newOffsetPolicy);
 		}
 		return null;
