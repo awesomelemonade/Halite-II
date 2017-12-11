e70b88bdb19382f59943aa69461015e108da0342
awesomelemonade <awesomelemonade@gmail.com>
2017-12-11 18:38:20 -0500

    Added Execution of FindProjectedDockedEnemyTask

--- NewBot/src/lemon/halite2/task/projection/FindProjectedDockedEnemyTask.java
+++ NewBot/src/lemon/halite2/task/projection/FindProjectedDockedEnemyTask.java
@@ -4,44 +4,132 @@ import hlt.GameConstants;
 import hlt.GameMap;
 import hlt.Move;
 import hlt.Planet;
+import hlt.RoundPolicy;
 import hlt.Ship;
+import hlt.ThrustMove;
+import hlt.ThrustPlan;
 import hlt.Vector;
 import lemon.halite2.pathfinding.Obstacle;
+import lemon.halite2.pathfinding.ObstacleType;
 import lemon.halite2.pathfinding.Pathfinder;
 import lemon.halite2.task.BlameMap;
 import lemon.halite2.task.Task;
 import lemon.halite2.util.BiMap;
+import lemon.halite2.util.MathUtil;
 
 public class FindProjectedDockedEnemyTask implements Task {
 	private Ship enemyShip;
 	private Vector projection;
 	private double distance;
+	private boolean activate;
 	public FindProjectedDockedEnemyTask(Ship enemyShip){
 		this.enemyShip = enemyShip;
 		//Find Projection
 		double bestDistance = Double.MAX_VALUE;
 		Vector bestProjection = null;
 		for(Planet planet: GameMap.INSTANCE.getPlanets()){
+			if(planet.isOwned()){
+				continue;
+			}
 			double distance = planet.getPosition().getDistanceTo(enemyShip.getPosition())-planet.getRadius()-GameConstants.SHIP_RADIUS;
 			if(distance<bestDistance){
 				bestDistance = distance;
 				bestProjection = planet.getPosition().addPolar(planet.getRadius()+GameConstants.SHIP_RADIUS, planet.getPosition().getDirectionTowards(enemyShip.getPosition()));
 			}
 		}
-		this.projection = bestProjection;
-		this.distance = bestDistance;
+		if(bestProjection==null){
+			this.activate = true;
+			this.projection = bestProjection;
+			this.distance = bestDistance;
+		}else{
+			this.activate = false;
+		}
 	}
 	@Override
 	public void accept(Ship ship) {
-		
+		this.activate = false;
 	}
 	@Override
 	public Move execute(Ship ship, Pathfinder pathfinder, BlameMap blameMap,
 			BiMap<Integer, Obstacle> uncertainObstacles) {
-		return null;
+		double direction = ship.getPosition().getDirectionTowards(projection);
+		double directionDegrees = Math.toDegrees(direction);
+		int roundedDegrees = RoundPolicy.ROUND.applyDegrees(direction);
+		int preferredSign = directionDegrees-((int)directionDegrees)<0.5?1:-1;
+		//try candidates
+		for(int i=7;i>0;--i) {
+			for(int j=0;j<=MathUtil.PI_DEGREES;++j) {
+				int candidateA = MathUtil.normalizeDegrees(roundedDegrees+j*preferredSign);
+				int candidateB = MathUtil.normalizeDegrees(roundedDegrees-j*preferredSign);
+				if(pathfinder.getCandidate(i, candidateA, ObstacleType.PERMANENT, ObstacleType.ENEMY)==null) {
+					Obstacle obstacle = pathfinder.getCandidate(i, candidateA, ObstacleType.UNCERTAIN);
+					if(obstacle==null) {
+						return new ThrustMove(ship.getId(), new ThrustPlan(i, candidateA));
+					}else {
+						blameMap.add(uncertainObstacles.getKey(obstacle), ship.getId());
+						return null;
+					}
+				}
+				if(pathfinder.getCandidate(i, candidateB, ObstacleType.PERMANENT, ObstacleType.ENEMY)==null) {
+					Obstacle obstacle = pathfinder.getCandidate(i, candidateB, ObstacleType.UNCERTAIN);
+					if(obstacle==null) {
+						return new ThrustMove(ship.getId(), new ThrustPlan(i, candidateB));
+					}else {
+						blameMap.add(uncertainObstacles.getKey(obstacle), ship.getId());
+						return null;
+					}
+				}
+			}
+		}
+		//try still candidate
+		if(pathfinder.getStillCandidate(ObstacleType.ENEMY)==null) {
+			Obstacle obstacle = pathfinder.getStillCandidate(ObstacleType.UNCERTAIN);
+			if(obstacle==null) {
+				return new ThrustMove(ship.getId(), new ThrustPlan(0, 0));
+			}else {
+				blameMap.add(uncertainObstacles.getKey(obstacle), ship.getId());
+				return null;
+			}
+		}
+		//try candidates
+		for(int i=7;i>0;--i) {
+			for(int j=0;j<=MathUtil.PI_DEGREES;++j) {
+				int candidateA = MathUtil.normalizeDegrees(roundedDegrees+j*preferredSign);
+				int candidateB = MathUtil.normalizeDegrees(roundedDegrees-j*preferredSign);
+				if(pathfinder.getCandidate(i, candidateA, ObstacleType.PERMANENT)==null) {
+					Obstacle obstacle = pathfinder.getCandidate(i, candidateA, ObstacleType.UNCERTAIN);
+					if(obstacle==null) {
+						return new ThrustMove(ship.getId(), new ThrustPlan(i, candidateA));
+					}else {
+						blameMap.add(uncertainObstacles.getKey(obstacle), ship.getId());
+						return null;
+					}
+				}
+				if(pathfinder.getCandidate(i, candidateB, ObstacleType.PERMANENT)==null) {
+					Obstacle obstacle = pathfinder.getCandidate(i, candidateB, ObstacleType.UNCERTAIN);
+					if(obstacle==null) {
+						return new ThrustMove(ship.getId(), new ThrustPlan(i, candidateB));
+					}else {
+						blameMap.add(uncertainObstacles.getKey(obstacle), ship.getId());
+						return null;
+					}
+				}
+			}
+		}
+		Obstacle obstacle = pathfinder.getStillCandidate(ObstacleType.UNCERTAIN);
+		if(obstacle==null) {
+			return new ThrustMove(ship.getId(), new ThrustPlan(0, 0));
+		}else {
+			blameMap.add(uncertainObstacles.getKey(obstacle), ship.getId());
+			return null;
+		}
 	}
 	@Override
 	public double getScore(Ship ship) {
+		if(activate){
+			double distanceSquared = projection.getDistanceSquared(ship.getPosition());
+			return -distanceSquared;
+		}
 		return -Double.MAX_VALUE;
 	}
 }
