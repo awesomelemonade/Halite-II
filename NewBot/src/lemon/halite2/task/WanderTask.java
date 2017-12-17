566fd5a8f771111ca9a36f1884928748cd7331af
awesomelemonade <awesomelemonade@gmail.com>
2017-12-17 09:25:19 -0500

    Made WanderTask deterministic for debugging purposes

--- NewBot/src/lemon/halite2/task/WanderTask.java
+++ NewBot/src/lemon/halite2/task/WanderTask.java
@@ -1,5 +1,8 @@
 package lemon.halite2.task;
 
+import java.util.Random;
+
+import hlt.GameMap;
 import hlt.Move;
 import hlt.Ship;
 import hlt.ThrustMove;
@@ -10,6 +13,16 @@ import lemon.halite2.pathfinding.Pathfinder;
 import lemon.halite2.util.BiMap;
 
 public class WanderTask implements Task {
+	private Random r;
+	public WanderTask() {
+		long hash = GameMap.INSTANCE.getWidth();
+		hash = hash*500+GameMap.INSTANCE.getHeight();
+		hash = hash*50+GameMap.INSTANCE.getPlanets().size();
+		hash = hash*4+GameMap.INSTANCE.getMyPlayerId();
+		hash = hash*4+GameMap.INSTANCE.getPlayers().size();
+		hash = hash*300+GameMap.INSTANCE.getTurnNumber();
+		r = new Random(hash);
+	}
 	@Override
 	public void accept(Ship ship) {
 		
@@ -17,8 +30,8 @@ public class WanderTask implements Task {
 	@Override
 	public Move execute(Ship ship, Pathfinder pathfinder, BlameMap blameMap,
 			BiMap<Integer, Obstacle> uncertainObstacles) {
-		int randomMagnitude = (int)(7*Math.random());
-		int randomDirection = (int)(360*Math.random());
+		int randomMagnitude = r.nextInt(8);
+		int randomDirection = r.nextInt(360);
 		if(pathfinder.getCandidate(randomMagnitude, randomDirection, ObstacleType.PERMANENT, ObstacleType.UNCERTAIN)==null) {
 			return new ThrustMove(ship.getId(), new ThrustPlan(randomMagnitude, randomDirection));
 		}
