e85d0306389e13615d6375e849838bbcb9cb124f
awesomelemonade <awesomelemonade@gmail.com>
2017-12-18 22:22:00 -0500

    Added checkAbandon to AbandonTask

--- NewBot/src/lemon/halite2/task/experimental/AbandonTask.java
+++ NewBot/src/lemon/halite2/task/experimental/AbandonTask.java
@@ -1,6 +1,8 @@
 package lemon.halite2.task.experimental;
 
+import hlt.GameMap;
 import hlt.Move;
+import hlt.Planet;
 import hlt.Ship;
 import lemon.halite2.pathfinding.Obstacle;
 import lemon.halite2.pathfinding.Pathfinder;
@@ -9,9 +11,26 @@ import lemon.halite2.task.Task;
 import lemon.halite2.util.BiMap;
 
 public class AbandonTask implements Task {
-	public AbandonTask(){
-		//Check if we should abandon getting #1
-		//Find targets of #3 and #4
+	private static final double FACTOR = 0.75;
+	private static boolean abandon = false;
+	public static void checkAbandon() {
+		if(GameMap.INSTANCE.getPlayers().size()==4) {
+			abandon = false;
+			int[] count = new int[4];
+			for(Planet planet: GameMap.INSTANCE.getPlanets()) {
+				if(planet.isOwned()) {
+					count[planet.getOwner()]++;
+				}
+			}
+			for(int i=0;i<count.length;++i) {
+				if(i==GameMap.INSTANCE.getMyPlayerId()) { //Don't abandon if you're winning!
+					continue;
+				}
+				if(((double)count[i])/((double)GameMap.INSTANCE.getPlanets().size())>FACTOR) {
+					abandon = true;
+				}
+			}
+		}
 	}
 	@Override
 	public void accept(Ship ship) {
@@ -24,6 +43,6 @@ public class AbandonTask implements Task {
 	}
 	@Override
 	public double getScore(Ship ship) {
-		return -Double.MAX_VALUE;
+		return abandon?Double.MAX_VALUE:-Double.MAX_VALUE;
 	}
 }
